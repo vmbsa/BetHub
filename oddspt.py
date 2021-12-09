40ccf021827a0dfc07a8ab0506dc135faca996c2
vmbsa-iscte <vmbsa@iscte-iul.pt>
2021-12-09 21:41:36 +0000

    json into mongodb + chromedriver fix

--- oddspt.py
+++ oddspt.py
@@ -1,12 +1,13 @@
+import json
+import time
+import pymongo
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import pandas as pd
-import time
 
-df = pd.DataFrame(columns=['Home', 'Away', 'V1', 'X', 'V2', 'Arbitrage'])
-writer = pd.ExcelWriter('demo.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-writer.save()
+client = pymongo.MongoClient("mongodb://vasco:rootadmin@bethub-shard-00-00.24hpv.mongodb.net:27017,bethub-shard-00-01.24hpv.mongodb.net:27017,bethub-shard-00-02.24hpv.mongodb.net:27017/BetHub?ssl=true&replicaSet=atlas-14c0oo-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client.jogos
+db.jogos.delete_many({})
+
 
 def scroll_all_down():
     lenOfPage = browser.execute_script(
@@ -22,13 +23,23 @@ def scroll_all_down():
 
 #def get_casa_aposta():
 
-
+def create_json_add_to_mongo(id, home, away, v1, x, v2, arbitrage):
+    object = {}
+    object['game_id'] = id
+    object['home'] = home
+    object['away'] = away
+    object['v1'] = v1
+    object['x'] = x
+    object['v2'] = v2
+    object['percentage'] = arbitrage
+    json_data = json.dumps(object)
+    db.jogos.insert_one(object)
 
 
 
 
 browser = webdriver.Chrome()
-url = 'https://odds.pt/competicao/brasil-brasileirao-serie-a/1/'
+url = 'https://pt.odds.dog/'
 browser.get(url)
 
 scroll_all_down()
@@ -36,15 +47,16 @@ scroll_all_down()
 page_content = browser.page_source
 site = BeautifulSoup(page_content, 'html.parser')
 lista_todos = site.find(id="ContentGames")
+
+id = 0
+
 boxes = lista_todos.find_all('div', attrs={'class': 'box center tableBox shadowBox'})
-for box in boxes:
-    for tr in box.find_all('tr'):
+for box in boxes:       #Por cada box de jogos que encontra
+    for tr in box.find_all('tr'):       #Por cada jogo que encontra
         if(tr.find('div', attrs={'class': 'teamsGame team1'}) and tr.find('div', attrs={'class': 'teamsGame team2'})):
 
             home = tr.find('div', attrs={'class': 'teamsGame team1'}).find('div', attrs={'class': 'listNameTeam'}).contents[0]
-            print(home)
             away = tr.find('div', attrs={'class': 'teamsGame team2'}).find('div', attrs={'class': 'listNameTeam'}).contents[0]
-            print(away)
 
             odds_vector = []
 
@@ -55,18 +67,13 @@ for box in boxes:
                     odd = float(td.contents[0].text.replace(" ",""))
                     odds_vector.append(odd)
 
-            print(odds_vector)
             v1 = odds_vector[0]
             x = odds_vector[1]
             v2 = odds_vector[2]
-
             arbitrage = (1/v1)+(1/x)+(1/v2)
-            print(arbitrage)
 
+            create_json_add_to_mongo(id, home, away, v1, x, v2, arbitrage)
+            id += 1
 
-            data = pd.DataFrame([[home, away, v1, x, v2, arbitrage]])
-            data.to_excel(writer, index=False)
-            writer.save()
-            print("------------------------------------")
 
 
